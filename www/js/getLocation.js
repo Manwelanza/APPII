2cb2a860f5f4efc21e667dfc61c5532816d9c27c
Manuel <Manwelanza@gmail.com>
2015-12-16 14:29:49 +0000

    todo mal geolocalizacion

--- www/js/getLocation.js
+++ www/js/getLocation.js
@@ -1,28 +1,43 @@
 var place;
 
 function onSuccessGeolocation (position) {
-	var latidude = position.coords.latitude;
+	var latitude = position.coords.latitude;
 	var longitude = position.coords.longitude;
-	alert("Chivato");
 	var address = new google.maps.LatLng(latitude, longitude);
+	/*var address = [latitude, longitude];
+	//{lat: -34, lng: 151} --> latitud y longitud google API
 	alert("Chivato3");
-	var coords = new google.maps.places.Autocomplete(address);
-	cords.setTypes(['geocode']);
-	google.maps.event.addListener(coords, 'place_changed', function () {
-		place = coords.getPlace();
-		if (!place.geometry) {
+	var autocomplete = new google.maps.places.Autocomplete(address);
+	alert("Chivato4");
+	autocomplete.setTypes(['geocode']);
+	alert (autocomplete);
+	//google.maps.event.addListener(autocomplete, 'place_changed', function () {
+		alert ("chivato5");
+		var aux = autocomplete.getPlace();
+		alert ("chivato6");
+		if (!aux.geometry) {
 			alert("Chivato2");
 			return ;
 		}
-		var address = '';
+		alert ("chivato7");
+		place = '';
 		if (place.address_components) {
-			address = [
-                (place.address_components[0] && place.address_components[0].short_name || ''),
-                (place.address_components[1] && place.address_components[1].short_name || ''),
-                (place.address_components[2] && place.address_components[2].short_name || '')
+			alert ("chivato8");
+			place = [
+                (aux.address_components[0] && aux.address_components[0].short_name || ''),
+                (aux.address_components[1] && aux.address_components[1].short_name || ''),
+                (aux.address_components[2] && aux.address_components[2].short_name || '')
                 ].join(' ');
 		}
-	});
+	//});
+	alert ("chivato9");*/
+	alert ("a");
+	var aux2 = new google.maps.LatLngBounds (address);
+	var aux = new google.maps.places.SearchBox ();
+	alert ("b");
+	aux.setBounds (aux2);
+	alert (aux.getBounds());
+	alert (aux.getPlaces());
 	alert (place);
 }
 
