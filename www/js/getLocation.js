4f8b9e2f15ebbf3a4de2e383ff5a14ca13fc23a0
Manuel <Manwelanza@gmail.com>
2015-12-16 12:05:26 +0000

    test geoLocation

---
+++ www/js/getLocation.js
@@ -0,0 +1,23 @@
+var place = null;
+var onSuccessGeolocation (position) {
+	var latidude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	var address = new google.maps.LatLng(latitude, longitude);
+	var coords = new google.maps.places.Autocomplete(address);
+	cords.setTypes(['geocode']);
+	google.maps.event.addListener(coords, 'place_changed', function () {
+		place = coords.getPlace();
+		if (!place.geometry) {
+			return ;
+		}
+		var address = '';
+		if (place.address_components) {
+			address = [
+                (place.address_components[0] && place.address_components[0].short_name || ''),
+                (place.address_components[1] && place.address_components[1].short_name || ''),
+                (place.address_components[2] && place.address_components[2].short_name || '')
+                ].join(' ');
+		}
+	});
+	alert (place);
+}
