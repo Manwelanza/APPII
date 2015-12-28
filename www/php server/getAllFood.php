57658fea5b66797b6304930cee1c59d27184d86e
Manuel <Manwelanza@gmail.com>
2015-12-28 12:05:11 +0000

    comprobacion del azucar y mejora del pais en la busqueda de la BD

--- www/php server/getAllFood.php
+++ www/php server/getAllFood.php
@@ -6,14 +6,17 @@
 	$resultado['enviado'] = $_POST;
 
 	//ANALIZAMOS LOS DATOS
-	/*if ($_POST['sugar'] == "sugarYes") {
-		$sugar = 100;
+	if ($_POST['sugar'] == "sugarLow") {
+		$sugar = 35; // Si azúcar bajo, no puede ser mayor que 35
+	}
+	elseif ($_POST['sugar'] == "sugarMod"){
+		$sugar = 50; // Si azúcar medio, no puede ser mayor que 50
 	}
 	else {
-		$sugar = 36;
+		$sugar = 1000; // Si azúcar alto, no importa, por lo tanto "+infinito"
 	}
 
-	if ($_POST['energy'] == "energyNormal") {
+	/*if ($_POST['energy'] == "energyNormal") {
 		$energy = 500;
 	}
 	else {
@@ -22,9 +25,30 @@
 
 	//$resultado['respuesta'] =  "Vas a tomar " .$sugar. " y " .$energy. " calorias";
 
-	$place = $_POST['place'];
+	
 	// CONSULTA SQL
-	$sql = 'SELECT * FROM FOOD WHERE place=\'' . $place . '\';';
+	if ($_POST['place'] == "World") {
+		$sql = 'SELECT * 
+			FROM FOOD 
+			WHERE NOT EXISTS ( 
+				SELECT *
+				FROM FOOD_INGREDIENT FO, INGREDIENT INGRE 
+				WHERE (FOOD.id_food=FO.id_food) AND (FO.id_ingredient=INGRE.id_ingredient) AND
+						(INGRE.sugar>' . $sugar . '));';
+	}
+	else {
+		$place = $_POST['place'];
+		$sql = 'SELECT * 
+			FROM FOOD 
+			WHERE (place=\'' . $place . '\') AND NOT EXISTS ( 
+				SELECT *
+				FROM FOOD_INGREDIENT FO, INGREDIENT INGRE 
+				WHERE (FOOD.id_food=FO.id_food) AND (FO.id_ingredient=INGRE.id_ingredient) AND
+						(INGRE.sugar>' . $sugar . '));';
+	}
+	/*$sql = 'SELECT * 
+			FROM FOOD 
+			WHERE place=\'' . $place . '\';';*/
 	$consulta1 = getArraySQL($sql);
 
 	// DEVOLVEMOS UN RESULTADO
